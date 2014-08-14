8e2e02bd043655af9433e30f36a22fd5fb72db85
vince-styling <lingyunxiao@qq.com>
2014-08-14 21:22:56 +0800

    append more about information.

---
+++ lib/khaleesi/about.rb
@@ -0,0 +1,17 @@
+module Khaleesi
+  def self.version
+    '0.0.1'
+  end
+  def self.author
+    'vince styling'
+  end
+  def self.site
+    'https://github.com/vince-styling/Khaleesi'
+  end
+  def self.about
+    puts 'Khaleesi is a site blog generator write by ruby.'
+    puts "site : #{self.site}."
+    puts "author : #{self.author}."
+    puts "version : #{self.version}."
+  end
+end
--- lib/khaleesi/version.rb
+++
@@ -1,5 +0,0 @@
-module Khaleesi
-  def self.version
-    '0.0.1'
-  end
-end
